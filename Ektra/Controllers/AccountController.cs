b6c0c16464d45d19b1bfbc17d81850d8ddad6e37
hFuad <gfbfuad@gmail.com>
2018-08-30 19:06:28 +0400

    Add name to artist.

--- Ektra/Controllers/AccountController.cs
+++ Ektra/Controllers/AccountController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using Ektra.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Ektra.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Ektra.ViewModels;
 
 namespace Ektra.Controllers
 {
@@ -151,7 +149,12 @@ namespace Ektra.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
---
+++ Ektra/ViewModels/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Ektra.ViewModels
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
